8fe28f391411d21c73da8b5c11794d38ea8d4a67
Sanjayraj013 <120366299+Sanjayraj013@users.noreply.github.com>
2023-01-10 11:31:14 +0530

    Add files via upload

---
+++ happy num.py
@@ -0,0 +1,12 @@
+def is_happy_num(n):
+    past=set()
+    while n !=1:
+        n = sum(int(i)**2 for i in str(n))
+        if n in past:
+            return False
+        past.add(n)
+    return True
+print(is_happy_num(49))
+print(is_happy_num(156))
+print(is_happy_num(139))
+
